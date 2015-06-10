c0858e249341a1383249b6c0e6195b0c11133fa2
Cecilia Fredriksson <c.fredrikssons@gmail.com>
2015-06-10 23:22:50 +0200

    added page for shoppingbag

---
+++ shoppingcart.php
@@ -0,0 +1,7 @@
+
+<ul>
+	<li>2x Bomullshandduk</li>
+	<li>1x Tvålmuggar</li>
+	<li>3x Kratta</li>
+</ul>
+
--- single-products.php
+++ single-products.php
@@ -3,11 +3,12 @@ if(have_posts()) {
 	while(have_posts()) {
 		the_post();
 ?>
-<div class="singel-product-box">
-	<div class="product-img">
+<div class="singel_product_box">
+	
+	<div class="product_img">
 		<?php the_post_thumbnail('medium-thumbnail'); ?>
 	</div>
-	<div class="product-info">
+	<div class="product_info">
 		<h1><?php echo the_title(); ?></h1>
 		<h2><?php echo the_content(); ?></h2>
 	</div>
