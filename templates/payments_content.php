6fcc9cea2f3f8f16fc189962311bd6173cf02fdf
Cecilia Fredriksson <c.fredrikssons@gmail.com>
2015-08-17 13:36:10 +0200

    added content to payment_content

--- templates/payments_content.php
+++ templates/payments_content.php
@@ -2,18 +2,40 @@
 <div class="payment_box">
 	
 	<h2>Välj ett betalsätt</h2>
-
-	<input type="radio" value="Faktura" class="radio"/>
-	<input type="radio" value="Betala i kassan" class="radio"/>
-
+	
+	<form>
+		<div class="radio_div">
+			<input type="radio" name="payment" value="" class="radio"/>
+			Faktura
+			<p>Klubbkortsnummer:</p>
+			<input type="text" name="clubnr" placeholder="xxxx-xxxx-xxxx" />
+		</div>
+		<div class="radio_div">
+			<input type="radio" name="payment" value="" class="radio"/>
+			Betala i kassan
+			<p>Inget klubbkort behövs <br/>
+			Betala kontant eller med kort
+			</p>
+		</div>
+	</form>
 </div>
 
 
 <div class="information_box">
 	
-	<h2>Välj ett betalsätt</h2>
+	<h2>Din information</h2>
 
-	<input type="radio" value="Faktura" class="radio"/>
-	<input type="radio" value="Betala i kassan" class="radio"/>
-
+	<form>
+		<div class="info">
+			Name: <input type="text" name="clubnr" placeholder="Epsilon" />
+			Address:<input type="text" name="clubnr" placeholder="epsilonvägen 13" />
+			Postnummer: <input type="text" name="clubnr" placeholder="133 77 Stockholm" />
+		</div>
+		<div class="info">
+			Email:<input type="text" name="clubnr" placeholder="epsilon@gmail.com" />
+			Telefonnr:<input type="text" name="clubnr" placeholder="073 133 777" />
+			Upprepa telefonnr:<input type="text" name="clubnr" placeholder="073 133 777" />
+		</div>
+	</form>
+	
 </div>
