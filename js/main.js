e294b87450d5b0d4873b1e3c622a6879892e9a8d
Elin Kahn <elinakahn@gmail.com>
2015-08-17 13:01:21 +0200

    added ajax-functions

--- js/main.js
+++ js/main.js
@@ -2,39 +2,10 @@
 "use strict";
 	
 	$(document).ready(function() {
-		// Load Products
-	   /*function loadProducts (container, href) {
-			$(container).load(href + ' #products');
-		}
-
-		$( '.product_categories' ).on( 'click', function (event) {
-			
-			var product_container = $('.products_container');
-			var href = $(this).attr('href');
-			var container = product_container.find('.product');
-
-			if ( !container.length ) {
-				 product_container.append('<div class="product"></div>');
-				 container = product_container.find('.product');
-			}
-
-			event.preventDefault();
-			loadProducts(container, href);
-			$('.close_single_btn').fadeIn('fast');
-
-		});
-
-		// Close products
-		$('.close_single_btn').on('click', function() { 
-
-			$('.product').remove();
-			$('.close_single_btn').css('display', 'none');
-			
-		});*/
 
 	function loadContent () {
 		$.ajax({
-			url: '/wp-admin/admin-ajax.php',
+			url: 'wp-admin/admin-ajax.php',
 			data: {
 				action: 'load_content'
 			}
