53690628669fe43cc0762412114075602f656605
Cecilia Fredriksson <c.fredrikssons@gmail.com>
2015-08-18 00:25:02 +0200

    added styling and markup for payment_content page

--- templates/payments_content.php
+++ templates/payments_content.php
@@ -1,41 +1,42 @@
+<div class="payment">
+	<div class="payment_box">
 
-<div class="payment_box">
-	
-	<h2>Välj ett betalsätt</h2>
-	
-	<form>
-		<div class="radio_div">
-			<input type="radio" name="payment" value="" class="radio"/>
-			Faktura
-			<p>Klubbkortsnummer:</p>
-			<input type="text" name="clubnr" placeholder="xxxx-xxxx-xxxx" />
-		</div>
-		<div class="radio_div">
-			<input type="radio" name="payment" value="" class="radio"/>
-			Betala i kassan
-			<p>Inget klubbkort behövs <br/>
-			Betala kontant eller med kort
-			</p>
-		</div>
-	</form>
-</div>
+		<h2>Välj ett betalsätt</h2>
+
+		<form>
+			<div class="radio_div">
+				<input type="radio" name="payment" value="" class="radio"/>
+				Faktura
+				<p>Klubbkortsnummer:</p>
+				<input type="text" name="clubnr" placeholder="xxxx-xxxx-xxxx-5434" size="25" class="input_text" />
+			</div>
+			<div class="radio_div">
+				<input type="radio" name="payment" value="" class="radio"/>
+				Betala i kassan
+				<p>Inget klubbkort behövs <br/>
+				Betala kontant eller med kort
+				</p>
+			</div>
+		</form>
+	</div>
 
 
-<div class="information_box">
-	
-	<h2>Din information</h2>
+	<div class="information_box">
 
-	<form>
-		<div class="info">
-			Name: <input type="text" name="clubnr" placeholder="Epsilon" />
-			Address:<input type="text" name="clubnr" placeholder="epsilonvägen 13" />
-			Postnummer: <input type="text" name="clubnr" placeholder="133 77 Stockholm" />
-		</div>
-		<div class="info">
-			Email:<input type="text" name="clubnr" placeholder="epsilon@gmail.com" />
-			Telefonnr:<input type="text" name="clubnr" placeholder="073 133 777" />
-			Upprepa telefonnr:<input type="text" name="clubnr" placeholder="073 133 777" />
-		</div>
-	</form>
-	
+		<h2>Din information</h2>
+
+		<form>
+			<div class="info">
+				Name: <input type="text" name="clubnr" placeholder="Epsilon" class="input_text" />
+				Address:<input type="text" name="clubnr" placeholder="epsilonvägen 13" class="input_text" />
+				Postnummer: <input type="text" name="clubnr" placeholder="133 77 Stockholm" class="input_text" />
+			</div>
+			<div class="info">
+				Email:<input type="text" name="clubnr" placeholder="epsilon@gmail.com" class="input_text" />
+				Telefonnr:<input type="text" name="clubnr" placeholder="073 133 777" class="input_text" />
+				Upprepa telefonnr:<input type="text" name="clubnr" placeholder="073 133 777" class="input_text" />
+			</div>
+		</form>
+
+	</div>
 </div>
