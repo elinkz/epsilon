8cb5f8dd0389aecb3dff6577593770c0fabb452e
Elin Kahn <elinakahn@gmail.com>
2015-08-17 13:38:01 +0200

    added styling to shopping cart page

--- templates/page_shoppingcart.php
+++ templates/page_shoppingcart.php
@@ -25,14 +25,19 @@ get_header();
 	<nav class="main_nav">
 		<a href="#"><i class="fa fa-search fa-3x"></i></a>
 		<a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/hm-logo4.svg" alt="logo" class="home_logo_img"></a>
-		<a href="<?php echo get_page_link(42); ?>"><i class="fa fa-shopping-cart fa-3x"></i></a>
+		<a href="<?php echo get_page_link(42);?>"><i class="fa fa-shopping-cart fa-3x"></i></a>
 	</nav>
 </header>
 
 <div class="shopping_cart_content">
-	<a href="#" class="varukorg">VARUKORG</a>
-	<a href="javascript:void(0);" class="betalsatt">BETALSÄTT</a>
-	<a href="#" class="tack">TACK FÖR DITT KÖP</a>
+	<div class="shopping_cart_header">
+		<a href="javascript:void(0);"><i class="fa fa-undo fa-3x"></i></a>
+		<a href="javascript:void(0);" class="varukorg cart_steps">VARUKORG</a>
+		<i class="fa fa-angle-right fa-2x angle_1"></i>
+		<a href="javascript:void(0);" class="betalsatt cart_steps step_1">BETALSÄTT</a>
+		<i class="fa fa-angle-right fa-2x angle_2"></i>
+		<a href="javascript:void(0);" class="kvitto cart_steps step_2">KVITTO</a>	
+	</div>
 </div>
 
 <?php get_footer(); ?>
