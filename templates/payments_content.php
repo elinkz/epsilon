008120be5364c7398fd2b71bc57323e4b2f15009
Cecilia Fredriksson <c.fredrikssons@gmail.com>
2015-08-17 12:47:17 +0200

    added 2 new files in template folder

---
+++ templates/payments_content.php
@@ -0,0 +1,19 @@
+
+<div class="payment_box">
+	
+	<h2>Välj ett betalsätt</h2>
+
+	<input type="radio" value="Faktura" class="radio"/>
+	<input type="radio" value="Betala i kassan" class="radio"/>
+
+</div>
+
+
+<div class="information_box">
+	
+	<h2>Välj ett betalsätt</h2>
+
+	<input type="radio" value="Faktura" class="radio"/>
+	<input type="radio" value="Betala i kassan" class="radio"/>
+
+</div>
