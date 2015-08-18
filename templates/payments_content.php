128edd4aa5124d8003597fc5f094a9207dfecdf9
Cecilia Fredriksson <c.fredrikssons@gmail.com>
2015-08-18 09:04:20 +0200

    styling and markup for payment_content page

--- templates/payments_content.php
+++ templates/payments_content.php
@@ -6,14 +6,14 @@
 		<form>
 			<div class="radio_div">
 				<input type="radio" name="payment" value="" class="radio"/>
-				Faktura
-				<p>Klubbkortsnummer:</p>
+				<p class="big_text">Faktura</p>
+				<p class="medium_text">Klubbkortsnummer:</p>
 				<input type="text" name="clubnr" placeholder="xxxx-xxxx-xxxx-5434" size="25" class="input_text" />
 			</div>
 			<div class="radio_div">
 				<input type="radio" name="payment" value="" class="radio"/>
-				Betala i kassan
-				<p>Inget klubbkort behövs <br/>
+				<p class="big_text">Betala i kassan</p>
+				<p class="medium_text">Inget klubbkort behövs <br/>
 				Betala kontant eller med kort
 				</p>
 			</div>
@@ -27,16 +27,20 @@
 
 		<form>
 			<div class="info">
-				Name: <input type="text" name="clubnr" placeholder="Epsilon" class="input_text" />
-				Address:<input type="text" name="clubnr" placeholder="epsilonvägen 13" class="input_text" />
-				Postnummer: <input type="text" name="clubnr" placeholder="133 77 Stockholm" class="input_text" />
+				Name: <input type="text" name="name" placeholder="Epsilon" class="input_text" />
+				Address:<input type="text" name="address" placeholder="epsilonvägen 13" class="input_text" />
+				Postnummer: <input type="text" name="postnr" placeholder="133 77 Stockholm" class="input_text" />
 			</div>
 			<div class="info">
-				Email:<input type="text" name="clubnr" placeholder="epsilon@gmail.com" class="input_text" />
-				Telefonnr:<input type="text" name="clubnr" placeholder="073 133 777" class="input_text" />
-				Upprepa telefonnr:<input type="text" name="clubnr" placeholder="073 133 777" class="input_text" />
+				Email:<input type="text" name="email" placeholder="epsilon@gmail.com" class="input_text" />
+				Upprepa email:<input type="text" name="email2" placeholder="073 133 777" class="input_text" />
+				Telefonnr:<input type="text" name="tel" placeholder="073 133 777" class="input_text" />
+				
 			</div>
 		</form>
+		<p>Bekräfta ditt köp</p>
+		<a href="<?php echo get_page_link(42); ?>"><i class="fa fa-chevron-right fa-2x"></i></a>
 
 	</div>
+	
 </div>
